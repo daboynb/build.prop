8a4ad987722a9ca2bec67fab1caa8b44776f65ea
daboynb <106079917+daboynb@users.noreply.github.com>
2024-08-15 21:55:19 +0200

    Update migrate_osmosis.sh

--- migrate_osmosis.sh
+++ migrate_osmosis.sh
@@ -49,20 +49,41 @@ grep_check_json api_level && [ ! "$FORCE" ] && die "No migration required";
 
 [ "$INSTALL" ] || item "Parsing fields ...";
 
-# Exclude INCREMENTAL, TYPE, TAGS, and ID from FPFIELDS
-FPFIELDS="BRAND PRODUCT DEVICE RELEASE"
-ALLFIELDS="MANUFACTURER MODEL FINGERPRINT $FPFIELDS SECURITY_PATCH"
+FPFIELDS="BRAND PRODUCT DEVICE RELEASE ID INCREMENTAL TYPE TAGS";
+ALLFIELDS="MANUFACTURER MODEL FINGERPRINT $FPFIELDS SECURITY_PATCH DEVICE_INITIAL_SDK_INT";
 
 for FIELD in $ALLFIELDS; do
   eval $FIELD=\"$(grep_get_json $FIELD)\";
 done;
 
-# Skip ID field processing
-#if [ -n "$ID" ] && ! grep_check_json build.id; then
-#  item 'Simple entry ID found, changing to ID field and "*.build.id" property ...';
-#fi;
+if [ -n "$ID" ] && ! grep_check_json build.id; then
+  item 'Simple entry ID found, changing to ID field and "*.build.id" property ...';
+fi;
 
-if [ -z "$RELEASE" -o "$OVERRIDE" ]; then
+if [ -z "$ID" ] && grep_check_json BUILD_ID; then
+  item 'Deprecated entry BUILD_ID found, changing to ID field and "*.build.id" property ...';
+  ID="$(grep_get_json BUILD_ID)";
+fi;
+
+if [ -n "$SECURITY_PATCH" ] && ! grep_check_json security_patch; then
+  item 'Simple entry SECURITY_PATCH found, changing to SECURITY_PATCH field and "*.security_patch" property ...';
+fi;
+
+if grep_check_json VNDK_VERSION; then
+  item 'Deprecated entry VNDK_VERSION found, changing to "*.vndk.version" property ...';
+  VNDK_VERSION="$(grep_get_json VNDK_VERSION)";
+fi;
+
+if [ -n "$DEVICE_INITIAL_SDK_INT" ] && ! grep_check_json api_level; then
+  item 'Simple entry DEVICE_INITIAL_SDK_INT found, changing to DEVICE_INITIAL_SDK_INT field and "*api_level" property ...';
+fi;
+
+if [ -z "$DEVICE_INITIAL_SDK_INT" ] && grep_check_json FIRST_API_LEVEL; then
+  item 'Deprecated entry FIRST_API_LEVEL found, changing to DEVICE_INITIAL_SDK_INT field and "*api_level" property ...';
+  DEVICE_INITIAL_SDK_INT="$(grep_get_json FIRST_API_LEVEL)";
+fi;
+
+if [ -z "$RELEASE" -o -z "$INCREMENTAL" -o -z "$TYPE" -o -z "$TAGS" -o "$OVERRIDE" ]; then
   if [ "$OVERRIDE" ]; then
     item "Overriding values for fields derivable from FINGERPRINT ...";
   else
@@ -83,6 +104,11 @@ if [ -z "$SECURITY_PATCH" -o "$SECURITY_PATCH" = "null" ]; then
   unset SECURITY_PATCH;
 fi;
 
+if [ -z "$DEVICE_INITIAL_SDK_INT" -o "$DEVICE_INITIAL_SDK_INT" = "null" ]; then
+  item 'Missing required DEVICE_INITIAL_SDK_INT field and "*api_level" property value found, setting to 25 ...';
+  DEVICE_INITIAL_SDK_INT=25;
+fi;
+
 if [ -f "$OUT" ]; then
   item "Renaming old file to $(basename "$OUT").bak ...";
   mv -f "$OUT" "$OUT.bak";
@@ -91,21 +117,12 @@ fi;
 [ "$INSTALL" ] || item "Writing fields and properties to updated custom.pif.json ...";
 
 (echo "{";
-first=true
 for FIELD in $ALLFIELDS; do
-  if [ "$first" = true ]; then
-    first=false
-  else
-    echo ","
-  fi
-  eval echo '\ \ \ \ \"$FIELD\": \"'\$$FIELD'\"'
+  eval echo '\ \ \ \ \"$FIELD\": \"'\$$FIELD'\",';
 done;
 if [ "$ADVANCED" ]; then
-  echo ",$N  // Advanced Settings";
-  echo '    "verboseLogs": "0"';
-fi
-echo "$N}"
-) > "$OUT";
-
+  echo "$N  // Advanced Settings";
+  echo '    "verboseLogs": "0",';
+fi) | sed '$s/,/\n}/' > "$OUT";
 
 [ "$INSTALL" ] || cat "$OUT";
