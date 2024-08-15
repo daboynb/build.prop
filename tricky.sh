9e996a5bdf8e4797a6b1b76c3b6fbf7d4456bf5c
daboynb <106079917+daboynb@users.noreply.github.com>
2024-08-15 16:36:01 +0200

    Update tricky.sh

--- tricky.sh
+++ tricky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Sovrascrivi il file se esiste
-> spoof_build_vars
+> edited_tricky.txt
 
 # Leggi e processa il file JSON
 while IFS= read -r line; do
@@ -11,11 +11,12 @@ while IFS= read -r line; do
   # Controlla se la riga non è vuota e contiene una coppia chiave-valore valida
   if [[ ! -z "$clean_line" && "$clean_line" =~ [A-Z_]+ ]]; then
     # Sostituisci il primo spazio con un segno di uguale per creare il formato chiave=valore
-    echo "$clean_line" | sed 's/ /=/' >> spoof_build_vars
+    echo "$clean_line" | sed 's/ /=/' >> edited_tricky.txt
   fi
 
   # Interrompi il processo dopo aver trovato SECURITY_PATCH
   if [[ "$clean_line" == SECURITY_PATCH* ]]; then
     break
   fi
-done < pif.json
+done < edited.json
+
